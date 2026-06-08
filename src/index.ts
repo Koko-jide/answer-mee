719320162b81bbee3c71d8bd4b173790da6f2067
Benedict Udemeh <benudemeh@gmail.com>
2026-06-09 00:10:26 +0100

    Added prisma components to the Backend

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main () {
+    const newUser = await prisma.user.create({
+        data: {
+            name: 'Alice',
+            email: 'alice@prisma.io',
+           },
+        })
+        console.log('Created user:', newUser)
+      }
+
+      main()
+        .catch((e) => console.error(e))
+        .finally(async () => await prisma.$disconnect())
+
