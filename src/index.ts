f5c54f854054d0756b58ed8293408ad3ab9e682e
Benedict Udemeh <benudemeh@gmail.com>
2026-06-10 06:49:08 +0100

    API routes created

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,41 @@
-import { PrismaClient } from '@prisma/client'
+import express, { type Request, type Response } from 'express';
+import { prisma } from './lib/prisma.js'; 
 
-const prisma = new PrismaClient()
 
-async function main () {
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); //Allows the server to read JSON from the request body
+
+// 1. GET all users
+app.get('/users', async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+// 2. POST a new user (Create)
+app.post('/users', async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+  try {
     const newUser = await prisma.user.create({
-        data: {
-            name: 'Alice',
-            email: 'alice@prisma.io',
-           },
-        })
-        console.log('Created user:', newUser)
-      }
+      data: { name, email },
+    });
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ error: "User already exists or invalid data" });
+  }
+});
 
-      main()
-        .catch((e) => console.error(e))
-        .finally(async () => await prisma.$disconnect())
+// 3. GET a specific user by ID
+app.get('/users/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const user = await prisma.user.findUnique({
+    where: { id: Number(id)},
+  });
+  if (user) res.json(user);
+  else res.status(404).json({ error: "User not found" });
+});
 
+app.listen(PORT, () => {
+  console.log('🚀 Server is running on http://localhost:${process.env.PORT || 3000}');
+});
